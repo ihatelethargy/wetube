2d5d90007f016e684fff40fe7ea56d62ec4096aa
ihatelethargy <naturalflavor1007@naver.com>
2020-07-23 19:33:44 +0900

    searchController

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from my home");
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localsMiddleware);
 
-app.get("/",handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
-
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
---
+++ controllers/videoController.js
@@ -0,0 +1,16 @@
+export const home = (req, res) => res.render("home",{pageTitle:"Home"});
+
+export const upload = (req, res) => res.render("upload", {pageTitle:"upload"});
+
+export const search = (req, res) => {
+    
+    const {query: { term:searchingBy }} = req;
+
+    res.render("search", {pageTitle:"search", searchingBy: searchingBy});
+}
+
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"videoDetail"});
+
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"editVideo"});
+
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle:"deleteVideo"});
